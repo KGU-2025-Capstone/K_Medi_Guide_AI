6ab9bbc88ab3dd3a45e6005e605a620c5a6caaa7
Recionds <56007790+Recionds@users.noreply.github.com>
2025-04-25 23:27:00 +0900

    [#13] refactor : api 요청 주소 변경
    #11 
    ### 📌 작업 개요
    - Flask 내 라우트들의 api 요청 주소를 변경합니다.
    
    ### 🔧 작업 배경
    - 추후 프론트와 통합 후에 api 요청을 잘 구분하기 위해 변경합니다.
    
    ### 🧾 변경 사항
    - app.py 파일 내부의 blueprint url을 /api/로 시작하게 변경했습니다.
    
    ### ✅ 테스트 및 확인
    - [x] 주요 예시 입력값에 대한 출력 결과 검증 완료
    - [x] 로컬 테스트 기준 이상 없음

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from routes import symptom, select, detail, name, start
 
 app = Flask(__name__)
-app.register_blueprint(symptom.bp, url_prefix='/medicine')
-app.register_blueprint(select.bp, url_prefix='/medicine')
-app.register_blueprint(detail.bp, url_prefix='/medicine')
-app.register_blueprint(name.bp, url_prefix='/medicine')
-app.register_blueprint(start.bp, url_prefix='/medicine') 
+app.register_blueprint(symptom.bp, url_prefix='/api/medicine')
+app.register_blueprint(select.bp, url_prefix='/api/medicine')
+app.register_blueprint(detail.bp, url_prefix='/api/medicine')
+app.register_blueprint(name.bp, url_prefix='/api/medicine')
+app.register_blueprint(start.bp, url_prefix='/api/medicine') 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=False)
